3d67eb3a56aed78429961da3f125f9ce516585bc
LoganTackett1 <logantackettwork@gmail.com>
2023-12-15 19:42:35 -0800

    gradient transition fixed transition bugs added theme toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import Topbar from './Topbar'
 
+const defaultTheme = 'light';
+
 function App() {
-  const [theme,setTheme] = useState('dark');
+  const [theme,setTheme] = useState(defaultTheme);
   const [cart,setCart] = useState([{title:"Dark Souls",quantity:2,id:"a1b2c3"},{title:"Elden Ring",quantity:3,id:"b1b2c3"}]);
 
   function changeTheme (theme) {
@@ -15,15 +17,25 @@ function App() {
     if (localStorage.getItem('theme')) {
       changeTheme(localStorage.getItem('theme'));
     } else {
-      changeTheme('dark');
+      changeTheme(defaultTheme);
     }
   },[]);
 
-  
+  let darkClass;
+  let lightClass;
+  if (theme === "dark") {
+    darkClass = "on";
+    lightClass = "off";
+  } else {
+    darkClass = "off";
+    lightClass = "on";
+  }
 
   return (
     <div id="app-container" className={`theme-${theme}`}>
-      <Topbar cart={cart} />
+      <div id="dark-background" className={'theme-dark' + ' ' + darkClass}></div>
+      <div id="light-background" className={'theme-light' + ' ' + lightClass}></div>
+      <Topbar cart={cart} changeTheme={changeTheme} theme={theme} />
       <div id="content-container"></div>
     </div>
   )
--- src/Topbar.jsx
+++ src/Topbar.jsx
@@ -1,11 +1,23 @@
+import PropTypes, { string } from 'prop-types';
 import './Topbar.css'
 
-export default function Topbar ({cart}) {
+export default function Topbar ({cart,changeTheme,theme}) {
+
+    function toggleTheme () {
+        if (theme === "dark") {
+            changeTheme("light");
+        } else {
+            changeTheme("dark");
+        }
+    }
 
     return (
         <div id='topbar-container'>
             <img id="logo" src="../public/white.webp" onError={() => {this.onerror=null; this.src='../public/white.png'}} alt="PixelPulse Logo" />
             <div id="nav-container"></div>
+            <div id="theme-switcher" onClick={toggleTheme}>
+                <div id="theme-ball"></div>
+            </div>
             <div id="cart-container">
                 <img id="cart-img" src="../public/cart.png" alt="Cart" />
                 {cart.length > 0 ? <div id="item-counter">{cart.length}</div> : ""}
@@ -13,3 +25,9 @@ export default function Topbar ({cart}) {
         </div>
     );
 }
+
+Topbar.propTypes = {
+    cart: PropTypes.array,
+    changeTheme: PropTypes.func,
+    theme: string
+};
