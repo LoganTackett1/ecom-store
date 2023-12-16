caf13ce290f5d87080a1ae9579513c0fa7c556b7
LoganTackett1 <logantackettwork@gmail.com>
2023-12-15 18:42:52 -0800

    cart button visual

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Topbar from './Topbar'
 
 function App() {
   const [theme,setTheme] = useState('dark');
+  const [cart,setCart] = useState([{title:"Dark Souls",quantity:2,id:"a1b2c3"},{title:"Elden Ring",quantity:3,id:"b1b2c3"}]);
 
   function changeTheme (theme) {
     localStorage.setItem('theme',theme);
@@ -22,7 +23,7 @@ function App() {
 
   return (
     <div id="app-container" className={`theme-${theme}`}>
-      <Topbar />
+      <Topbar cart={cart} />
       <div id="content-container"></div>
     </div>
   )
--- src/Topbar.jsx
+++ src/Topbar.jsx
@@ -1,12 +1,15 @@
 import './Topbar.css'
 
-export default function Topbar () {
-    
+export default function Topbar ({cart}) {
+
     return (
         <div id='topbar-container'>
-            <img id="logo" src="../public/white.webp" alt="" />
+            <img id="logo" src="../public/white.webp" onError={() => {this.onerror=null; this.src='../public/white.png'}} alt="PixelPulse Logo" />
             <div id="nav-container"></div>
-            <div id="cart-container"></div>
+            <div id="cart-container">
+                <img id="cart-img" src="../public/cart.png" alt="Cart" />
+                {cart.length > 0 ? <div id="item-counter">{cart.length}</div> : ""}
+            </div>
         </div>
     );
 }
