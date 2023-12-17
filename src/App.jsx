58088f66ae4010a16b47b404486e2734e65bd08a
LoganTackett1 <logantackettwork@gmail.com>
2023-12-17 03:05:34 -0800

    finished Slider has flickering issue

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ function App() {
       <div id="light-background" className={'theme-light' + ' ' + lightClass}></div>
       <Topbar cart={cart} changeTheme={changeTheme} theme={theme} />
       <div id="content-container">
-        <Slider num={3} prefix="test" cardWidth={80} containerWidth={360} grow={false}>
+        <Slider num={3} prefix="test" cardWidth={80} containerWidth={360} grow={true} delay={200}>
           <div id="test1"></div>
           <div id="test2"></div>
           <div id="test3"></div>
--- src/Slider.jsx
+++ src/Slider.jsx
@@ -11,9 +11,22 @@ import './Slider.css';
         Card Wrapper: prefix-card-wrapper
 
     Gap is the pixel gap between card wrappers
+
+    To give the slider transition effects use these classes:
+    Hidden card (Removed on click): prefix-hidden
+    Card to be hidden (Added on click): prefix-hiding
+    Card general during transition: prefix-transition
+    Card left vs right transformations: prefix-transform-<left/right>
+
+    Delay is the time given to transition before new render (ms)
+    Use the css var --translate when making translation transitions
 */
 
-export default function Slider ({children,num,prefix,cardWidth,containerWidth,grow}) {
+function mod(n, m) {
+    return ((n % m) + m) % m;
+  }
+
+export default function Slider ({children,num,prefix,cardWidth,containerWidth,grow,delay}) {
     const [card,setCard] = useState(0);
     const [childArr,setChildArr] = useState([]);
 
@@ -23,44 +36,86 @@ export default function Slider ({children,num,prefix,cardWidth,containerWidth,gr
         setChildArr(arr);
     },[children]);
 
+    let gap;
+    if (grow) {
+        gap = (containerWidth-(num*cardWidth))/(num+1);
+    } else {
+        gap = (containerWidth-(num*cardWidth))/(num-1);
+    }
+
+    useEffect(() => {
+        const sliderContainer = document.getElementsByClassName(prefix+'-sl-container');
+        sliderContainer[0].style.setProperty('--translate',`${gap+cardWidth}px`);
+    });
+
     function getCurrElements () {
         const currArr = [];
         for (let i = -1; i <= num; i++) {
-            currArr.push(childArr[(card+i)%childArr.length]);
+            currArr.push(childArr[mod(card+i,childArr.length)]);
         }
         return currArr;
     }
 
     function applyAnimations (dir) {
-        const cardWrappers = [...document.getElementsByClassName("card-wrapper")];
+        const cardWrappers = [...document.getElementsByClassName(prefix+"-card-wrapper")];
+        if (dir == "left") {
+            cardWrappers[0].classList.remove(prefix+"-hidden");
+            cardWrappers[cardWrappers.length-2].classList.add(prefix+"-hiding");
+        } else {
+            cardWrappers[cardWrappers.length-1].classList.remove(prefix+"-hidden");
+            cardWrappers[1].classList.add(prefix+"-hiding");
+        }
         for (let i = 0; i < cardWrappers.length; i++) {
+            cardWrappers[i].classList.add(prefix+"-transition");
+            if (dir == "left") {
+                cardWrappers[i].classList.add(prefix+"-transform-left");
+            } else {
+                cardWrappers[i].classList.add(prefix+"-transform-right");
+            }
+        }
+    }
 
+    function removeAnimations () {
+        const cardWrappers = [...document.getElementsByClassName(prefix+"-card-wrapper")];
+        for (let i = 0; i < cardWrappers.length; i++) {
+            cardWrappers[i].classList.remove(prefix+"-transition");
+            cardWrappers[i].classList.remove(prefix+"-transform-left");
+            cardWrappers[i].classList.remove(prefix+"-transform-right");
+            if (i > 0 && i < cardWrappers.length-1) {
+                cardWrappers[i].classList.remove(prefix+"-hiding");
+                cardWrappers[i].classList.remove(prefix+"-hidden");
+            } else {
+                cardWrappers[i].classList.remove(prefix+"-hiding");
+                cardWrappers[i].classList.add(prefix+"-hidden");
+            }
         }
     }
 
     function buttonClick (dir) {
         if (dir == "left") {
-            setCard(card - 1);
+            applyAnimations("left");
+            setTimeout(() => {
+                removeAnimations();
+                setCard(mod(card-1,childArr.length));
+            },delay);
         } else {
-            setCard(card + 1);
+            applyAnimations("right");
+            setTimeout(() => {
+                removeAnimations();
+                setCard(mod(card+1,childArr.length));
+            },delay);
         }
     }
-
+    
     return (
         <div id="slider-container" className={prefix+"-sl-container"}>
             <button className={"sliderBtn" + " " + prefix+"-sl-btn"} id="leftBtn" onClick={() => {buttonClick('left')}}>{"<"}</button>
             <div id="cards-container" className={prefix+"-cards-container"}>
                 {getCurrElements().map((element,index) => {
                     let classname = "";
-                    let gap;
-                    if (grow) {
-                        gap = (containerWidth-(num*cardWidth))/(num+1);
-                    } else {
-                        gap = (containerWidth-(num*cardWidth))/(num-1);
-                    }
 
-                    if (index == 0 || index == num) {
-                        classname = "hidden";
+                    if (index == 0 || index == num + 1) {
+                        classname = prefix+"-hidden";
                     }
 
                     return (
@@ -81,5 +136,6 @@ Slider.propTypes = {
     prefix: string,
     cardWidth: PropTypes.number,
     containerWidth: PropTypes.number,
-    grow: PropTypes.bool
+    grow: PropTypes.bool,
+    delay: PropTypes.number
 }
